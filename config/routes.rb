654b7f648769ad5daecc92b82768dd0bcd88c415
jive <tj111710@gmail.com>
2018-03-01 06:47:55 +0000

    add comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   root to: 'pages#home'
   
-  resources :articles
-  
+  resources :articles do
+    resources :comments
+  end
 end
